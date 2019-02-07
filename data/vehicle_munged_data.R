95c7d5c48b299e160805d5d62427038baef0c4ab
c-buchanan <41310320+c-buchanan@users.noreply.github.com>
2019-02-06 22:07:01 -0500

    adding vehicle sqlite database

---
+++ data/vehicle_munged_data.R
@@ -0,0 +1,64 @@
+# In this document, we are going to clean a csv file 
+
+# List the required libraries 
+library(tibble)
+library(tidyverse)
+library(DBI)
+library(plyr)
+library(RSQLite)
+library(dplyr)
+library(sqldf)
+
+# Read the CSV file into RStudio 
+raw_vehicle_registration <- read.csv(file="state_motor_vehicles_registrations_2012.csv", header=TRUE, na.strings="—")
+class(raw_vehicle_registration)
+
+# Remove the extraneous columns using dplyr by keeping the columns we want
+keep_columns <- select(raw_vehicle_registration, ï..STATE, TOTAL_AUTOMOBILES,
+                      BUSES_TOTAL, TOTAL_TRUCKS, MOTORCYCLES_TOTAL, 
+                      ALL_MOTOR_VEHICLES_TOTAL)
+
+# Rename Columns using the dplyr library 
+keep_columns <- rename(keep_columns, replace = c("ï..STATE" = "State", "TOTAL_AUTOMOBILES"="Automobiles",
+                       "BUSES_TOTAL"="Buses", "TOTAL_TRUCKS"="Trucks",
+                       "MOTORCYCLES_TOTAL"="Motorcycles", 
+                        "ALL_MOTOR_VEHICLES_TOTAL"="Total_Vehicles"))
+                       
+# Delete the redundant "Total" row 52 and District of Columbia row 9
+remove_total <- keep_columns[-c(52),] 
+vehicle_data <- remove_total[-c(9),] 
+
+# Check current data frame
+vehicle_data
+
+# Write cleaned data to a new CSV file
+write.csv(vehicle_data, file = "cleaned_vehicle_data.csv", row.names = FALSE)
+
+# Create the SQLite Database
+# Establish connection to database using RSQLite 
+db <- dbConnect(RSQLite::SQLite(), dbname = "Vehicle_Database.sqlite")
+
+# Create the empty Vehicle table
+dbSendQuery(conn = db,
+            "CREATE TABLE Vehicle
+            (State TEXT,
+            Automobiles INTEGER,
+            Buses INTEGER,
+            Trucks INTEGER,
+            Motorcycles INTEGER,
+            Total_Vehicles INTEGER)")
+
+# dbRemoveTable(db, "Vehicle")   # Remove the tables
+
+# Read the cleaned CSV into R 
+data <- read_csv("cleaned_vehicle_data.csv")  
+
+# Import data frames into database
+dbWriteTable(conn = db, name = "Vehicle", value = data, row.names = FALSE, append = TRUE)
+
+dbListTables(db)                 # The table in the database
+dbListFields(db, "Vehicle")       # The columns in the table
+dbReadTable(db, "Vehicle")        # The data in the table
+
+# Close the connection to the database 
+dbDisconnect(db) 
