23babca7829009ace7b192654308f7ad54fe7097
T McBeth <mcbethtyler@gmail.com>
2019-02-11 19:29:05 -0500

    Updated with Conor's stuff

--- static/js/biomass_methane.js
+++ static/js/biomass_methane.js
@@ -1,81 +1,91 @@
-function makeplot() {
-    d3.csv("methane_df.csv", function(data){ processData(data) } );
+// function makeplot() {
+//     d3.csv("methane_df.csv", function(data){ processData(data) } );
 
-};
+// };
     
-function processData(allRows) {
+// function processData(allRows) {
 
-    console.log(allRows);
-    var state = [], Tons_of_Greenhouse_Gas_Emissions = [], methane_emission = [], CO2_emissions =[], biomass_generation =[], transportation_generation =[];
+//     console.log(allRows);
+//     var state = [], Tons_of_Greenhouse_Gas_Emissions = [], methane_emission = [], CO2_emissions =[], biomass_generation =[], transportation_generation =[];
 
-    for (var i=0; i<allRows.length; i++) {
-        row = allRows[i];
-        state.push( row['State'] );
-        Tons_of_Greenhouse_Gas_Emissions.push( row['Tons of Greenhouse Gas Emissions'] );
-        methane_emission.push( row['Methane Emission'] );
-        CO2_emissions.push( row['CO2 Emissions'] );
-        biomass_generation.push( row['State Biomass Generation'] );
-        transportation_generation.push( row['State Transportation Generation'] );
-    }
-    console.log( 'A',state, 'B',Tons_of_Greenhouse_Gas_Emissions, 'C',methane_emission, 'D',CO2_emissions, 'E',biomass_generation, 'F',transportation_generation);
-    makePlotly( state, methane_emission, biomass_generation, CO2_emissions);
-}
+//     for (var i=0; i<allRows.length; i++) {
+//         row = allRows[i];
+//         state.push( row['State'] );
+//         Tons_of_Greenhouse_Gas_Emissions.push( row['Tons of Greenhouse Gas Emissions'] );
+//         methane_emission.push( row['Methane Emission'] );
+//         CO2_emissions.push( row['CO2 Emissions'] );
+//         biomass_generation.push( row['State Biomass Generation'] );
+//         transportation_generation.push( row['State Transportation Generation'] );
+//     }
+//     console.log( 'A',state, 'B',Tons_of_Greenhouse_Gas_Emissions, 'C',methane_emission, 'D',CO2_emissions, 'E',biomass_generation, 'F',transportation_generation);
+//     makePlotly( state, methane_emission, biomass_generation, CO2_emissions);
+// }
 
-function makePlotly( state, methane_emission, biomass_generation, CO2_emissions  ){
+function makePlotly() {
+  
+  var conor2chart = "/state_emission2";
+  d3.json(conor2chart).then(function (response) {
+    var state = response.state;
+    var transportation_generation = response.transportation_generation;
+    var methane_emission = response.methane_emission;
+    var CO2_emissions = response.CO2_emissions;
+    var biomass_generation = response.biomass_generation;
+
     var trace1 = {
-        x: state,
-        y: biomass_generation,
-        name: 'Biomass Generation',
-        type: 'scatter'
-      };
+      x: state,
+      y: biomass_generation,
+      name: 'Biomass Generation',
+      type: 'scatter'
+    };
       
-      var trace2 = {
-        x: state,
-        y: methane_emission,
-        name: 'Methane Emissions',
-        yaxis: 'y2',
-        type: 'scatter'
-      };
+    var trace2 = {
+      x: state,
+      y: methane_emission,
+      name: 'Methane Emissions',
+      yaxis: 'y2',
+      type: 'scatter'
+    };
       
-      var trace3 = {
-        x: state,
-        y: CO2_emissions,
-        name: 'CO2 Emissions',
-        yaxis: 'y3',
-        type: 'scatter'
-      };
+    var trace3 = {
+      x: state,
+      y: CO2_emissions,
+      name: 'CO2 Emissions',
+      yaxis: 'y3',
+      type: 'scatter'
+    };
 
-      var data = [trace1, trace2, trace3];
+    var data = [trace1, trace2, trace3];
       
-      var layout = {
-        title: 'Methane VS CO2 levels based off Biomass Generation',
-        width: 1200,
-        xaxis: {domain: [0.0, 0.8]},
-        yaxis: {
-          title: 'Biomass Produciton',
-          titlefont: {color: '#1f77b4'},
-          tickfont: {color: '#1f77b4'}
-        },
-        yaxis2: {
-          title: 'Methane Emissions',
-          titlefont: {color: '#ff7f0e'},
-          tickfont: {color: '#ff7f0e'},
-          anchor: 'free',
-          overlaying: 'y',
-          side: 'right',
-          position: 0.9
-        },
-        yaxis3: {
-          title: 'CO2 Emissions',
-          titlefont: {color: '#d62728'},
-          tickfont: {color: '#d62728'},
-          anchor: 'x',
-          overlaying: 'y',
-          side: 'right'
-        }
-      };
+    var layout = {
+      title: 'Methane VS CO2 levels based off Biomass Generation',
+      width: 1200,
+      xaxis: { domain: [0.0, 0.8] },
+      yaxis: {
+        title: 'Biomass Production',
+        titlefont: { color: '#1f77b4' },
+        tickfont: { color: '#1f77b4' }
+      },
+      yaxis2: {
+        title: 'Methane Emissions',
+        titlefont: { color: '#ff7f0e' },
+        tickfont: { color: '#ff7f0e' },
+        anchor: 'free',
+        overlaying: 'y',
+        side: 'right',
+        position: 0.9
+      },
+      yaxis3: {
+        title: 'CO2 Emissions',
+        titlefont: { color: '#d62728' },
+        tickfont: { color: '#d62728' },
+        anchor: 'x',
+        overlaying: 'y',
+        side: 'right'
+      }
+    };
       
-      Plotly.newPlot('conor2', data, layout);
-Plotly.newPlot('conor2', data, layout);
+    Plotly.newPlot('conor2', data, layout);
+
+  });
 };
 makePlotly();
