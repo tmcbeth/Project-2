24ddefe0c8c1c396435e5633f548cf3aebabd482
T McBeth <mcbethtyler@gmail.com>
2019-02-12 11:31:17 -0500

    added chart.js chart

--- static/js/logicChartjs.js
+++ static/js/logicChartjs.js
@@ -9,13 +9,106 @@ function makePlotly1() {
         var total_vehicles = response.total_vehicles;
        
         var bubble_chart = document.getElementById("bubble-chart");
-        var vehicle_bubble = new Chart(bubble_chart, {
+
+        console.log("Methane Emissions:", methane_emission);
+        console.log("CO2_emissions:", CO2_emissions);
+        console.log("total_vehicles:", total_vehicles);
+
+        methane_emissions_parsed = []
+
+        methane_emission.forEach(function (data) {
+            data = data.replace(/,/g, "")
+            parseInt(data, 10)
+            data = +data
+            methane_emissions_parsed.push(data)
+        });
+
+        CO2_emissions_parsed = []
+
+        CO2_emissions.forEach(function (data) {
+            data = data.replace(/,/g, "")
+            parseInt(data, 10)
+            data = +data
+            CO2_emissions_parsed.push(data)
+        });
+
+        total_vehicles_parsed = []
+
+        total_vehicles.forEach(function (data) {
+            // data = data.replace(/,/g, "")
+            // parseInt(data, 10)
+            data = data/1000000
+            total_vehicles_parsed.push(data)
+        });
+
+        console.log("Meth parsed", methane_emissions_parsed);
+        console.log("CO2 Parsed", CO2_emissions_parsed);
+        console.log("total_vehicles_parsed", total_vehicles_parsed);
+
+        var chartData = [];
+
+        methane_emissions_parsed.forEach(function (e, i) {
+            chartData.push({
+                x: e,
+                y: CO2_emissions_parsed[i],
+                r: total_vehicles_parsed[i],
+            })
+        })
+
+        console.log("Chart Data", chartData)
+        
+        var popCanvas = document.getElementById("bubble-chart");
+
+        Chart.defaults.global.defaultFontFamily = "Arial";
+        Chart.defaults.global.defaultFontSize = 14;
+
+        // var popData = {
+        //     datasets: [{
+        //         label: ['Circle Size: Total Number of Vehicles'],
+        //         data: [{
+        //             x: 100,
+        //             y: 0,
+        //             r: 10
+        //         }, {
+        //             x: 60,
+        //             y: 30,
+        //             r: 20
+        //         }, {
+        //             x: 40,
+        //             y: 60,
+        //             r: 25
+        //         }, {
+        //             x: 80,
+        //             y: 80,
+        //             r: 50
+        //         }, {
+        //             x: 20,
+        //             y: 30,
+        //             r: 25
+        //         }, {
+        //             x: 0,
+        //             y: 100,
+        //             r: 5
+        //         }],
+        //         backgroundColor: "#FF9966",
+      
+        
+        //     }]
+        // };
+
+        var bubbleChart = new Chart(popCanvas, {
             type: 'bubble',
-            data: data,
+            data: {
+                datasets: [{
+                    label: ['Circle Size: Total Number of Vehicles'],
+                    data: chartData,
+                    backgroundColor: "#FF9966",
+                }],
+            },
             options: {
                 title: {
                     display: true,
-                    text: 'Total Vehicles by State and Gas Emissions'
+                    text: 'Total Vehicles by State vs Gas Emissions'
                 }, scales: {
                     yAxes: [{
                         scaleLabel: {
@@ -29,29 +122,17 @@ function makePlotly1() {
                             labelString: "Methane Emissions",
                         }
                     }]
-                }
+                },
+        
             },
-
-            label: ["Total Vehicles"],
-            backgroundColor: "rgba(193,46,12,0.2)",
-            borderColor: "rgba(193,46,12,1)",
-            data: [{
-                // X Value
-                x: methane_emission,
-                // Y Value
-                y: CO2_emissions,
-                // Bubble radius in pixels (not scaled).
-                r: total_vehicles,
-                    }],
-           
-               
+  
         });
-        
-        
-        
-                
-
     });
 }
 
+        
+        
+        
+        
 makePlotly1();
+
