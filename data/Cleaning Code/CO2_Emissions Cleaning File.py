8ab938389fdb2925b60405823d4119168219943a
hoyos321 <hoyos321@gmail.com>
2019-02-11 20:15:21 -0500

    Added files from KH

--- data/Cleaning Code/CO2_Emissions Cleaning File.py
+++ data/Cleaning Code/CO2_Emissions Cleaning File.py
@@ -2,123 +2,68 @@ import os
 import pandas as pd
 import numpy as np
 
+import csv
+import json
+
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
-import sqlite3
-import pymysql
-pymysql.install_as_MySQLdb()
+from sqlalchemy import create_engine, Column, Integer, ForeignKey, Numeric, DateTime, func
 
-from flask import Flask, jsonify, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-
-#################################################
-# Database Setup
-#################################################
 
 # import & clean 1996 Emissions file
 emissions96 = "data/egrid1996.csv"
 emissions96 = pd.read_csv(emissions96)
-emissions96 = emissions96.rename(columns={"PSTATABB\nState abbreviation": "State", "STCO2AN\nState 1996 annual CO2 emissions (tons)": "CO2_Emissions_1996"})
-emissions96_final = emissions96[["State", "CO2_Emissions_1996"]]
+emissions96 = emissions96.rename(columns={
+    "STHTIAN\nState 1996 annual heat input (MMBtu)": "Ann_Heat_Input_1996",
+    "STCO2AN\nState 1996 annual CO2 emissions (tons)": "CO2_Emissions_1996",
+    "STCO2RTA\nState average 1996 annual CO2 output emission rate (lbs/MWh)": "Ann_CO2_Rate_1996"})
+emissions96_final = emissions96[["State", "Ann_Heat_Input_1996", "Ann_CO2_Rate_1996", "CO2_Emissions_1996"]]
 # print(list(emissions96_final.columns.values))
 
 # import & clean 2016 Emissions file
 emissions16 = "data/egrid2016.csv"
 emissions16 = pd.read_csv(emissions16)
-emissions16 = emissions16.rename(columns={"State abbreviation": "State", "State annual CO2 emissions (tons)": "CO2_Emissions_2016"})
-emissions16_final = emissions16[["State", "CO2_Emissions_2016"]]
+emissions16 = emissions16.rename(columns={
+    "State total annual heat input (MMBtu)": "Ann_Heat_Input_2016",
+    "State annual CO2 emissions (tons)": "CO2_Emissions_2016",
+    "State annual CO2 total output emission rate (lb/MWh)": "Ann_CO2_Rate_2016"})
+emissions16_final = emissions16[["State", "Ann_Heat_Input_2016", "Ann_CO2_Rate_2016", "CO2_Emissions_2016"]]
 # print(list(emissions16.columns.values))
 
-# # merge the two datasets together
+# merge the two datasets together
 CO2_Emissions_df = pd.merge(emissions96_final, emissions16_final, on='State')
-# print(list(CO2_Emissions.columns.values))
+CO2_Emissions_df.CO2_Emissions_2016 = CO2_Emissions_df.CO2_Emissions_2016.astype(int)
+CO2_Emissions_df.Ann_CO2_Rate_2016 = CO2_Emissions_df.Ann_CO2_Rate_2016.astype(float)
+CO2_Emissions_df.Ann_Heat_Input_2016 = CO2_Emissions_df.Ann_Heat_Input_2016.astype(float)
+CO2_Emissions_df.set_index("State", inplace=True)
+
+# add a difference column
+difference = CO2_Emissions_df["CO2_Emissions_2016"]-CO2_Emissions_df["CO2_Emissions_1996"]
+CO2_Emissions_df["CO2_Difference"] = difference
+
+#calculate and print summary stats of 1996 and 2016 data
+print(CO2_Emissions_df["CO2_Emissions_1996"].mean())
+print(CO2_Emissions_df["CO2_Emissions_2016"].mean())
+print(CO2_Emissions_df["Ann_CO2_Rate_1996"].mean())
+print(CO2_Emissions_df["Ann_CO2_Rate_2016"].mean())
+print(CO2_Emissions_df["Ann_Heat_Input_1996"].mean())
+print(CO2_Emissions_df["Ann_Heat_Input_2016"].mean())
+
+#reorder columns & convert merged table to csv
+CO2_Emissions_df = CO2_Emissions_df[["Ann_Heat_Input_1996", "Ann_Heat_Input_2016", "Ann_CO2_Rate_1996", "Ann_CO2_Rate_2016", "CO2_Emissions_1996","CO2_Emissions_2016", "CO2_Difference"]]
+CO2_Emissions_df.to_csv("data/Final_CO2_Emissions.csv")
 
 #convert merged table into an SQLite database
 engine = create_engine("sqlite:///db/CO2_Emissions.sqlite")
 CO2_Emissions_df.to_sql("CO2_Emissions", engine, if_exists="replace", index = True)
-conn = sqlite3.connect("CO2_Emissions.db")
 
-# CO2_Emissions = SQLAlchemy(app)
-# conn.execute("SHOW TABLES;")
+#jsonify data
+csvfile = open("data/Final_CO2_Emissions.csv", 'r')
+jsonfile = open("CO2_Emissions.json", 'w')
 
-# #create flask homepage
-# @app.route("/")
-# def index():
-#     """Return the homepage."""
-#     return render_template("index.html")
-
-# @app.route("/names")
-# def names():
-#     """Return a list of sample names."""
-
-#     # Use Pandas to perform the sql query
-#     results = db.session.query(CO2_Emissions).all()
-
-#     # Return a list of the column names (sample names)
-#     trace = [result[0] for result in results]
-
-#     return jsonify(trace)
-
-# @app.route("/livestock")
-# def livestock_data():
-#     """Return all livestock data"""
-#     results = db.session.query(livestock.State, livestock.Commodity, livestock.DataItem, livestock.Value).\
-#         order_by(livestock.State).all()
-
-#     state = [result[0] for result in results]
-#     commodity = [result[1] for result in results]
-#     livestock_desc = [result[2] for result in results]
-#     total = [result[3] for result in results]
-
-#     trace = {
-#         "state": state,
-#         "commodity": commodity,
-#         "livestock_desc": livestock_desc,
-#         "Total": total
-#     }
-
-#     return jsonify(trace)
-
-# @app.route("/animal_totals")
-# def animal_totals():
-#     """Return all livestock data"""
-
-#     results = db.session.query(livestock.Commodity, func.sum(livestock.Value)).\
-#         group_by(livestock.Commodity).all()
-
-#     commodity = [result[0] for result in results]
-#     total = [result[1] for result in results]
-
-
-#     trace = {
-#         "commodity": commodity,
-#         "Total": total
-#     }
-
-#     return jsonify(trace)
-
-# @app.route("/by_state/<commodity>")
-# def state_totals(commodity):
-#     """Return all livestock data"""
-
-#     results = db.session.query(livestock.State, livestock.Value).\
-#         filter(livestock.Commodity == func.upper(commodity)).all()
-
-#     state = [result[0] for result in results]
-#     total = [result[1] for result in results]
-
-
-#     trace = {
-#         "state": state,
-#         "Inventory": total
-#     }
-
-#     return jsonify(trace)
-
-
-# if __name__ == "__main__":
-#     app.run()
+fieldnames = ("State","Ann_Heat_Input_1996", "Ann_Heat_Input_2016", "Ann_CO2_Rate_1996", "Ann_CO2_Rate_2016", "CO2_Emissions_1996","CO2_Emissions_2016", "CO2_Difference")
+reader = csv.DictReader(csvfile, fieldnames)
+for row in reader:
+    json.dump(row, jsonfile)
+    jsonfile.write('\n')
---
+++ static/js/logic-CO2.js
@@ -0,0 +1,74 @@
+function makeplot() {
+    d3.csv("data/Final_CO2_Emissions.csv", function(data){
+        processData(data)});
+};
+ 
+function processData(allRows) {
+ 
+    // console.log(allRows);
+    var Ann_Heat_Input_1996 = [], Ann_Heat_Input_2016 = [], Ann_CO2_Rate_1996 = [], Ann_CO2_Rate_2016 = [], CO2_Emissions_1996 = [], CO2_Emissions_2016 = [], State = [], CO2_Difference = [];
+ 
+    for (var i=0; i<allRows.length; i++) {
+        row = allRows[i];
+        Ann_Heat_Input_1996.push(row['Ann_Heat_Input_1996']);
+        Ann_Heat_Input_2016.push(row['Ann_Heat_Input_2016']);
+        Ann_CO2_Rate_1996.push(row['Ann_CO2_Rate_1996']);
+        Ann_CO2_Rate_2016.push(row['Ann_CO2_Rate_2016']);
+        CO2_Emissions_1996.push(row['CO2_Emissions_1996']);
+        CO2_Emissions_2016.push(row['CO2_Emissions_2016']);
+        CO2_Difference.push(row['CO2_Difference']);
+        State.push(row['State']);
+    }
+    console.log('a',Ann_Heat_Input_1996, 'b',Ann_Heat_Input_2016, 'c',Ann_CO2_Rate_1996, 'd',Ann_CO2_Rate_2016, 'e',CO2_Emissions_1996, 'f',CO2_Emissions_2016, 'g',CO2_Difference, 'h',State);
+    makePlotly1(CO2_Difference, State);
+    makePlotly2(Ann_Heat_Input_1996, Ann_Heat_Input_2016, Ann_CO2_Rate_1996, Ann_CO2_Rate_2016, CO2_Emissions_1996, CO2_Emissions_2016);
+};
+ 
+function makePlotly1(CO2_Difference, State){
+    var plotDiv = document.getElementById("Kate1");
+    var traces = [{
+        x: State,
+        y: CO2_Difference,
+        type: 'bar'
+    }];
+    
+    var data = [traces];
+    
+    var layout = {
+        title: "Difference in CO2 Emissions (1996 vs. 2016)"
+      };
+ 
+    Plotly.newPlot('Kate1', traces,
+       {title: 'Difference in CO2 Emissions (1996 vs. 2016)'});
+};
+
+makeplot();
+
+ 
+function makePlotly2(){
+    var plotDiv = document.getElementById("Kate2");
+    var trace1 = {
+        x: ['Avg_Ann_Heat_Input', 'Avg_Ann_CO2_Rate', 'Avg_CO2_Emissions'],
+        y: [461173748.6960785/1000000, 1467.0758823529404, 2291060309/1000000],
+        name: '1996',
+        type: 'bar'
+      };
+          
+    var trace2 = {
+        x: ['Avg_Ann_Heat_Input', 'Avg_Ann_CO2_Rate', 'Avg_CO2_Emissions'],
+        y: [748714312.0784314/1000000, 990.5294117647059, 2034489329/1000000],
+        name: '2016',
+        type: 'bar'
+      };
+          
+    var data = [trace1, trace2];
+          
+    var layout = {
+        barmode: 'group',
+        title: "Changes Over Time (1996 vs. 2016)"
+      };
+          
+    Plotly.newPlot('Kate2', data, layout);
+};
+
+makeplot();
